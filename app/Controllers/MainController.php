7ae93a76bfff123ad668fd1afc03b6f932dc2e5e
dankenethrontale <127752262+Danken082@users.noreply.github.com>
2023-09-28 21:50:48 +0800

    main

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class MainController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function nice()
+    {
+        return view('view');
+    }
+}
